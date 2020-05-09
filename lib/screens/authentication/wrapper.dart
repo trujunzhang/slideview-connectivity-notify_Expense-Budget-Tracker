3cf705b7c62cbf63228e4ce56692eea39db9e688
djzhang <trujunzhang@gmail.com>
2020-05-09 17:16:20 +0800

    https://github.com/chirag10k/Expense-Budget-Tracker

---
+++ lib/screens/authentication/wrapper.dart
@@ -0,0 +1,19 @@
+import 'package:ieatta/enums/connectivity_status.dart';
+import 'package:ieatta/models/user_model.dart';
+import 'package:ieatta/screens/authentication/authenticate_screen.dart';
+import 'package:ieatta/screens/home/home_screen.dart';
+import 'package:ieatta/screens/offline_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class Wrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final user = Provider.of<User>(context);
+    final connectionStatus = Provider.of<ConnectivityStatus>(context);
+    //return either Home or Authenticate Screen
+    return (connectionStatus == ConnectivityStatus.Offline)
+        ? OfflineScreen()
+          : ((user != null) ? HomeScreen() : AuthenticateScreen());
+  }
+}
---
+++ lib/services/connectivity_service.dart
@@ -0,0 +1,27 @@
+import 'dart:async';
+
+import 'package:ieatta/enums/connectivity_status.dart';
+import 'package:connectivity/connectivity.dart';
+
+class ConnectivityService{
+
+  StreamController<ConnectivityStatus> connectionStatusController = StreamController<ConnectivityStatus>();
+
+  Stream<ConnectivityStatus> get onConnectivityChanged {
+    return Connectivity().onConnectivityChanged.map(_getStatusFromResult);
+  }
+
+  ConnectivityStatus _getStatusFromResult(ConnectivityResult result) {
+    switch(result){
+      case ConnectivityResult.mobile:
+        return ConnectivityStatus.Cellular;
+      case ConnectivityResult.wifi:
+        return ConnectivityStatus.WiFi;
+      case ConnectivityResult.none:
+        return ConnectivityStatus.Offline;
+      default :
+        return ConnectivityStatus.Offline;
+    }
+  }
+
+}
